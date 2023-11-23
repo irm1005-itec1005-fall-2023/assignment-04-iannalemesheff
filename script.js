330f4ad09cbcaefd2b3a3a29d2588fbfe32e8873
Ianna Lemesheff <iannalemesheff@cmail.carleton.ca>
2023-11-23 00:10:02 -0500

    finishedProduct

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-
-//
-// Variables
-//
-
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+function addTask(){
+  if(inputBox.value === ''){
+    alert("scary virus pop up");
   }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
 }
 
-//
-// Inits & Event Listeners
-//
-inititialise();
+listContainer.addEventListener("click", function(e) {
+  if(e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  }
+  else if(e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+inputBox.addEventListener("keypress", function(event){
+  if (event.key === "Enter") {
+    document.getElementById("add-button").click();
+  }
+});
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
